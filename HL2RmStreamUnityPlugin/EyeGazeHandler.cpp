3e7660ac8ff841ccd1429865c53b0ab6e90376e7
jondebug <jonathanpaul202@gmail.com>
2022-05-17 13:05:21 +0300

    some more changes from friday morning

--- HL2RmStreamUnityPlugin/EyeGazeHandler.cpp
+++ HL2RmStreamUnityPlugin/EyeGazeHandler.cpp
@@ -10,6 +10,9 @@
 #include <winrt/Windows.Foundation.Collections.h>
 #include <winrt/Windows.Web.Syndication.h>
 #include "EyeGazeHandler.h"
+#include <winrt/Windows.Storage.h>
+#include <fstream>
+#include <iomanip>
 
 using namespace winrt::Windows::Foundation;
 using namespace winrt::Windows::Media::Capture;
@@ -18,27 +21,165 @@ using namespace winrt::Windows::Graphics::Imaging;
 using namespace winrt::Windows::Perception::Spatial;
 using namespace winrt::Windows::Networking::Sockets;
 using namespace winrt::Windows::Storage::Streams;
+using namespace DirectX;
+using namespace winrt::Windows::Storage;
 
 
 
+EyeGazeStreamer::EyeGazeStreamer()
+{
+	// reserve for 10 seconds at 60fps
+	m_hethateyeLog.reserve(10 * 60);
+}
+
+void EyeGazeStreamer::AddFrame(HeTHaTEyeFrame&& frame)
+{
+	m_hethateyeLog.push_back(std::move(frame));
+}
+
+void EyeGazeStreamer::Clear()
+{
+	m_hethateyeLog.clear();
+}
+
+size_t EyeGazeStreamer::FrameCount() const
+{
+	return m_hethateyeLog.size();
+}
+
+const std::vector<HeTHaTEyeFrame>& EyeGazeStreamer::Log() const
+{
+	return m_hethateyeLog;
+}
+
+std::ostream& operator<<(std::ostream& out, const XMMATRIX& m)
+{
+	XMFLOAT4X4 mView;
+	XMStoreFloat4x4(&mView, m);
+	for (int j = 0; j < 4; ++j)
+	{
+		for (int i = 0; i < 4; ++i)
+		{
+			out << std::setprecision(8) << mView.m[i][j];
+			if (i < 3 || j < 3)
+			{
+				out << ",";
+			}
+		}
+	}
+	return out;
+}
+
+std::ostream& operator<<(std::ostream& out, const XMVECTOR& v)
+{
+	XMFLOAT4 mView;
+	XMStoreFloat4(&mView, v);
+
+	out << std::setprecision(8) << mView.x << "," << mView.y << "," << mView.z << "," << mView.w;
+
+	return out;
+}
+
+void DumpHandIfPresentElseZero(bool present, const XMMATRIX& handTransform, std::ostream& out)
+{
+	static const float zeros[16] = { 0.0 };
+	static const XMMATRIX zero4x4(zeros);
+	if (present)
+	{
+		out << handTransform;
+	}
+	else
+	{
+		out << zero4x4;
+	}
+}
 
 void DumpEyeGazeIfPresentElseZero(bool present, const XMVECTOR& origin, const XMVECTOR& direction, float distance, std::ostream& out)
 {
 	XMFLOAT4 zeros{ 0, 0, 0, 0 };
 	XMVECTOR zero4 = XMLoadFloat4(&zeros);
 	out << present << ",";
-	/*if (present)
+	if (present)
 	{
-		
 		out << origin << "," << direction;
 	}
 	else
 	{
 		out << zero4 << "," << zero4;
 	}
-	out << "," << distance; */
+	out << "," << distance;
 }
 
+
+bool EyeGazeStreamer::DumpTransformToDisk(const XMMATRIX& mtx, const StorageFolder& folder, const std::wstring& datetime_path, const std::wstring& suffix) const
+{
+	auto path = folder.Path().data();
+	std::wstring fullName(path);
+	fullName += +L"\\" + datetime_path + suffix;
+	std::ofstream file(fullName);
+	if (!file)
+	{
+		return false;
+	}
+	file << mtx;
+	file.close();
+	return true;
+}
+bool EyeGazeStreamer::DumpToDisk(const StorageFolder& folder, const std::wstring& datetime_path) const
+{
+	auto path = folder.Path().data();
+	std::wstring fullName(path);
+	fullName += +L"\\" + datetime_path + L"_head_hand_eye.csv";
+	std::ofstream file(fullName);
+	if (!file)
+	{
+		return false;
+	}
+
+	for (const HeTHaTEyeFrame& frame : m_hethateyeLog)
+	{
+		file << frame.timestamp << ",";
+		file << frame.headTransform;
+		file << ",";
+		file << frame.leftHandPresent;
+		for (int j = 0; j < (int)HandJointIndex::Count; ++j)
+		{
+			file << ",";
+			DumpHandIfPresentElseZero(frame.leftHandPresent, frame.leftHandTransform[j], file);
+		}
+		file << ",";
+		file << frame.rightHandPresent;
+		for (int j = 0; j < (int)HandJointIndex::Count; ++j)
+		{
+			file << ",";
+			DumpHandIfPresentElseZero(frame.rightHandPresent, frame.rightHandTransform[j], file);
+		}
+		file << ",";
+		DumpEyeGazeIfPresentElseZero(frame.eyeGazePresent, frame.eyeGazeOrigin, frame.eyeGazeDirection, frame.eyeGazeDistance, file);
+		file << std::endl;
+	}
+	file.close();
+	return true;
+}
+/*
+void DumpEyeGazeIfPresentElseZero(bool present, const XMVECTOR& origin, const XMVECTOR& direction, float distance, std::ostream& out)
+{
+	XMFLOAT4 zeros{ 0, 0, 0, 0 };
+	//XMVECTOR zero4 = XMLoadFloat4(&zeros);
+	out << present << ",";
+	if (present)
+	{
+		
+		out << origin.n128_f32[0] << "," << origin.n128_f32[1] << "," << origin.n128_f32[2] << "," << origin.n128_f32[3]
+			<< "," << direction.n128_f32[0] << "," << direction.n128_f32[1] << "," << direction.n128_f32[2] << "," << direction.n128_f32[3];
+	}
+	else
+	{
+		out << 0 << "," << 0 << "," << 0 << "," << 0 << "," << 0 << "," << 0 << "," << 0 << "," << 0;
+	}
+	out << "," << distance; 
+}
+*/
 void EyeGazeStreamer::OnConnectionReceived(
 	StreamSocketListener /* sender */,
 	StreamSocketListenerConnectionReceivedEventArgs args)
@@ -63,6 +204,7 @@ void EyeGazeStreamer::OnConnectionReceived(
 		OutputDebugStringW(L"\n");
 	}
 }
+
 IAsyncAction EyeGazeStreamer::StartEyeStreamServer()
 {
 	try
@@ -107,11 +249,77 @@ IAsyncAction EyeGazeStreamer::InitializeAsync(
 	m_streamingEnabled = true;
 
 	StartEyeStreamServer();
-	m_pStreamThread = new std::thread(EyeStreamThread, this);
+	m_pStreamThread = new std::thread(EyeGazeStreamer::GetAndSendThread, this);
+	//m_pStreamThread = new std::thread(EyeStreamThread, this);
+	//m_pEyeCollenctionThread  = new std::thread(GetEyeGazeThread, this);
 	//m_OnFrameArrivedRegistration = mediaFrameReader.FrameArrived({ this, &VideoCameraStreamer::OnFrameArrived });
 }
+
+std::wstring SetDateTimePath()
+{
+	std::wstring  datetime;
+	wchar_t m_datetime_c[200];
+	const std::time_t now = std::time(nullptr);
+	std::tm tm;
+	if (localtime_s(&tm, &now))
+	{
+		return false;
+	}
+	if (!std::wcsftime(m_datetime_c, sizeof(m_datetime_c), L"%F-%H%M%S", &tm))
+	{
+		return false;
+	}
+	datetime.assign(m_datetime_c);
+	return datetime;
+}
+
+void EyeGazeStreamer::DumpEyesToFile() {
+
+	StorageFolder localFolder = ApplicationData::Current().LocalFolder();
+	auto archiveSourceFolder = co_await localFolder.CreateFolderAsync(
+		L"archiveSource",
+		CreationCollisionOption::ReplaceExisting);
+	if (archiveSourceFolder)
+	{
+		auto m_archiveFolder = archiveSourceFolder;
+		std::wstring datetime = SetDateTimePath();
+		DumpToDisk(m_archiveFolder, datetime);
+	}
+
+}
+
+void EyeGazeStreamer::GetAndSendThread(EyeGazeStreamer* pProcessor) {
+	OutputDebugString(L"EyeStreamer::CameraStreamThread: Starting eye streaming thread.\n");
+	while (!pProcessor->m_fExit)
+	{
+		//std::lock_guard<std::shared_mutex> reader_guard(m_frameMutex);
+
+		HeTHaTEyeFrame frame = pProcessor->ReturnEyeGazeFrame();
+		long long timestamp = frame.timestamp;
+		//long long timestamp = pProcessor->m_latestFrame.timestamp;
+		//long long timestamp = pStreamer->m_converter.RelativeTicksToAbsoluteTicks(
+		//	 HundredsOfNanoseconds(frame.SystemRelativeTime().Value().count())).count();
+
+		if (timestamp != pProcessor->m_latestTimestamp)
+		{
+			long long delta = timestamp - pProcessor->m_latestTimestamp;
+			if (delta > pProcessor->m_minDelta)
+			{
+				pProcessor->AddFrame(std::move(frame));
+				pProcessor->m_latestTimestamp = timestamp;
+				pProcessor->SendFrame(frame, timestamp);
+				pProcessor->m_writeInProgress = false;
+			}
+		}
+	}	
+
+
+}
+
+/*
  void EyeGazeStreamer::EyeStreamThread(EyeGazeStreamer* pStreamer) {
 	 OutputDebugString(L"EyeStreamer::CameraStreamThread: Starting eye streaming thread.\n");
+	 long EyeFrameCounter = 0;
 	 while (!pStreamer->m_fExit)
 	 {
 		 std::lock_guard<std::shared_mutex> reader_guard(pStreamer->m_frameMutex);
@@ -135,6 +343,7 @@ IAsyncAction EyeGazeStreamer::InitializeAsync(
 		 }
 	 }
 }
+*/
  void EyeGazeStreamer::SendFrame(
 	 HeTHaTEyeFrame pFrame, long long pTimestamp)
  {
@@ -149,43 +358,104 @@ IAsyncAction EyeGazeStreamer::InitializeAsync(
 		 OutputDebugStringW(L"Streamer::SendFrame: Streaming disabled.\n");
 		 return;
 	 }
+	 auto prevTimestamp = pFrame.timestamp;
+	 auto absoluteTimestamp = m_converter.RelativeTicksToAbsoluteTicks(HundredsOfNanoseconds((long long)prevTimestamp)).count();
+
 
 	 //m_writer.WriteBytes();
-	 
- }
+	 //std::vector<BYTE> EyeByteData;
+	 auto ptr = reinterpret_cast<BYTE*>(&pFrame);
+	 auto EyeByteData = std::vector<BYTE>(ptr, ptr + sizeof pFrame);
+	 m_writeInProgress = false;
 
- HeTHaTEyeFrame getEyegaze() {
+	 try
+	 {	 
+		 m_writer.WriteBytes(EyeByteData);
+		 OutputDebugStringW(L"Streamer::SendFrame: Trying to store writer...\n");
+		 m_writer.StoreAsync();
+	 }
+	 catch (winrt::hresult_error const& ex)
+	 {
+		 SocketErrorStatus webErrorStatus{ SocketError::GetStatus(ex.to_abi()) };
+		 if (webErrorStatus == SocketErrorStatus::ConnectionResetByPeer)
+		 {
+			 // the client disconnected!
+			 m_writer == nullptr;
+			 m_streamSocket == nullptr;
+			 m_writeInProgress = false;
+		 }
+		 winrt::hstring message = ex.message();
+		 OutputDebugStringW(L"Streamer::SendFrame: Sending failed with ");
+		 OutputDebugStringW(message.c_str());
+		 OutputDebugStringW(L"\n");
+	 }
+	 OutputDebugStringW(L"Streamer::SendFrame: Frame sent!\n");
+	 //this is how we rebuild the data on the client side: auto p_obj = reinterpret_cast<obj_t*>(&buffer[0]);
+ }
+ /*
+ void GetEyeGazeThread(EyeGazeStreamer Streamer) {
 
 	HeTHaTEyeFrame frame;
 
-	MixedReality m_mixedReality;
-	m_mixedReality.EnableMixedReality();
-	m_mixedReality.EnableEyeTracking();
-	m_mixedReality.Update();
-	const XMVECTOR headPosition = m_mixedReality.GetHeadPosition();
-	const XMVECTOR headForward = m_mixedReality.GetHeadForwardDirection();
-	const XMVECTOR headUp = m_mixedReality.GetHeadUpDirection();
-	frame.timestamp = m_mixedReality.GetPredictedDisplayTime();
+	Streamer.m_mixedReality.EnableMixedReality();
+	Streamer.m_mixedReality.EnableEyeTracking();
+	while (true) {
+		Streamer.m_mixedReality.Update();
+		const XMVECTOR headPosition = Streamer.m_mixedReality.GetHeadPosition();
+		const XMVECTOR headForward = Streamer.m_mixedReality.GetHeadForwardDirection();
+		const XMVECTOR headUp = Streamer.m_mixedReality.GetHeadUpDirection();
+		frame.timestamp = Streamer.m_mixedReality.GetPredictedDisplayTime();
 
-	if (m_mixedReality.IsEyeTrackingEnabled() && m_mixedReality.IsEyeTrackingActive())
-	{
-		frame.eyeGazePresent = true;
-		frame.eyeGazeOrigin = m_mixedReality.GetEyeGazeOrigin();
-		frame.eyeGazeDirection = m_mixedReality.GetEyeGazeDirection();
-		frame.eyeGazeDistance = 0.0f;
-		// Use surface mapping to compute the distance the user is looking at
-		if (m_mixedReality.IsSurfaceMappingActive())
+		if (Streamer.m_mixedReality.IsEyeTrackingEnabled() && Streamer.m_mixedReality.IsEyeTrackingActive())
 		{
-			float distance;
-			XMVECTOR normal;
-			if (m_mixedReality.GetSurfaceMappingInterface()->TestRayIntersection(frame.eyeGazeOrigin, frame.eyeGazeDirection, distance, normal))
+			frame.eyeGazePresent = true;
+			frame.eyeGazeOrigin = Streamer.m_mixedReality.GetEyeGazeOrigin();
+			frame.eyeGazeDirection = Streamer.m_mixedReality.GetEyeGazeDirection();
+			frame.eyeGazeDistance = 0.0f;
+			// Use surface mapping to compute the distance the user is looking at
+			if (Streamer.m_mixedReality.IsSurfaceMappingActive())
 			{
-				frame.eyeGazeDistance = distance;
+				float distance;
+				XMVECTOR normal;
+				if (Streamer.m_mixedReality.GetSurfaceMappingInterface()->TestRayIntersection(frame.eyeGazeOrigin, frame.eyeGazeDirection, distance, normal))
+				{
+					frame.eyeGazeDistance = distance;
+				}
 			}
 		}
 	}
-
 	//DumpEyeGazeIfPresentElseZero(frame.eyeGazePresent, frame.eyeGazeOrigin, frame.eyeGazeDirection, frame.eyeGazeDistance, file);
-	return frame;
+	//return frame;
 }
+
+*/
+ HeTHaTEyeFrame EyeGazeStreamer::ReturnEyeGazeFrame() {
+
+	 HeTHaTEyeFrame frame;
+
+	 m_mixedReality.Update();
+	 const XMVECTOR headPosition = m_mixedReality.GetHeadPosition();
+	 const XMVECTOR headForward = m_mixedReality.GetHeadForwardDirection();
+	 const XMVECTOR headUp = m_mixedReality.GetHeadUpDirection();
+	 frame.timestamp = m_mixedReality.GetPredictedDisplayTime();
+
+	 if (m_mixedReality.IsEyeTrackingEnabled() && m_mixedReality.IsEyeTrackingActive())
+	 {
+		 frame.eyeGazePresent = true;
+		 frame.eyeGazeOrigin = m_mixedReality.GetEyeGazeOrigin();
+		 frame.eyeGazeDirection = m_mixedReality.GetEyeGazeDirection();
+		 frame.eyeGazeDistance = 0.0f;
+		 // Use surface mapping to compute the distance the user is looking at
+		 if (m_mixedReality.IsSurfaceMappingActive())
+		 {
+			 float distance;
+			 XMVECTOR normal;
+			 if (m_mixedReality.GetSurfaceMappingInterface()->TestRayIntersection(frame.eyeGazeOrigin, frame.eyeGazeDirection, distance, normal))
+			 {
+				 frame.eyeGazeDistance = distance;
+			 }
+		 }
+	 }
+	 return frame;
+ }
 
--- HL2RmStreamUnityPlugin/EyeGazeHandler.h
+++ HL2RmStreamUnityPlugin/EyeGazeHandler.h
@@ -10,6 +10,7 @@
 #include <winrt/Windows.Foundation.Collections.h>
 #include <winrt/Windows.Web.Syndication.h>
 #include "EyeGazeHandler.h"
+__declspec(align(16))
 struct HeTHaTEyeFrame
 {
 	DirectX::XMMATRIX headTransform;
@@ -28,36 +29,48 @@ class EyeGazeStreamer
 {
 
 public:
-	EyeGazeStreamer()
-	{
-	}
+	EyeGazeStreamer();
 
 	virtual ~EyeGazeStreamer()
 	{
 		m_fExit = true;
+		DumpEyesToFile();
 		m_pStreamThread->join();
+		m_pEyeCollenctionThread->join();
 	}
+
+	const std::vector<HeTHaTEyeFrame>& Log() const;
+	size_t FrameCount() const;
+	void AddFrame(HeTHaTEyeFrame&& frame);
+	void Clear();
+	bool DumpToDisk(const winrt::Windows::Storage::StorageFolder& folder, const std::wstring& datetime_path) const;
+	bool DumpTransformToDisk(const DirectX::XMMATRIX& mtx, const winrt::Windows::Storage::StorageFolder& folder,
+		const std::wstring& datetime_path, const std::wstring& suffix) const;
+
 
 	winrt::Windows::Foundation::IAsyncAction InitializeAsync(
 		const long long minDelta,
 		const winrt::Windows::Perception::Spatial::SpatialCoordinateSystem& coordSystem,
 		std::wstring portName);
 
+	MixedReality m_mixedReality;
+	bool m_fExit = false;
+
 protected:
 	//void OnFrameArrived(const winrt::Windows::Media::Capture::Frames::MediaFrameReader& sender,
 	//	const winrt::Windows::Media::Capture::Frames::MediaFrameArrivedEventArgs& args);
 
 private:
-
+	HeTHaTEyeFrame ReturnEyeGazeFrame();
 	winrt::Windows::Foundation::IAsyncAction StartEyeStreamServer();
 
 	void OnConnectionReceived(
 		winrt::Windows::Networking::Sockets::StreamSocketListener /* sender */,
 		winrt::Windows::Networking::Sockets::StreamSocketListenerConnectionReceivedEventArgs args);
 
-	static void EyeStreamThread(EyeGazeStreamer* pProcessor);
-
+	void DumpEyesToFile();
 	void SendFrame(HeTHaTEyeFrame pFrame,	long long pTimestamp);
+	static void GetAndSendThread(EyeGazeStreamer* pProcessor);
 
 	//void WriteMatrix4x4(
 	//	_In_ winrt::Windows::Foundation::Numerics::float4x4 matrix);
@@ -71,7 +84,7 @@ private:
 
 	// streaming thread
 	std::thread* m_pStreamThread = nullptr;
-	bool m_fExit = false;
+	std::thread* m_pEyeCollenctionThread = nullptr;
 
 	bool m_streamingEnabled = true;
 
@@ -90,6 +103,5 @@ private:
 
 	static const int kImageWidth;
 	static const wchar_t kSensorName[3];
-
-
+	std::vector<HeTHaTEyeFrame> m_hethateyeLog;
 };
